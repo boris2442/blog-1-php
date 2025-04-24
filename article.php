160f72e0d1228935d5e2d9242999716e94e32558
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-24 12:25:52 +0200

    create show files

--- article.php
+++ article.php
@@ -1,3 +1,30 @@
 <?php
 session_start();
 require_once 'database/database.php';
+
+
+//recuperation des articles dans la datyabase...
+$sql="SELECT * FROM articles ORDER BY created_at DESC ";
+$query = $pdo->prepare($sql);
+$query->execute();
+$articles = $query->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+
+// 1--On affiche le titre autre
+
+$pageTitle ='Articles du Blog'; 
+
+// 2-Debut du tampon de la page de sortie
+ 
+ob_start();
+
+// 3-inclure le layout de la page d' show article
+require_once 'layouts/articles/show_html.php';
+
+//4-recuperation du contenu du tampon de la page d'accueil
+$pageContent = ob_get_clean();
+
+//5-Inclure le layout de la page de sortie
+require_once 'layouts/layout_html.php';
