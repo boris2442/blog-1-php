2cb656a6d4b0ee7d9b7eea67049acdc6fca84f73
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-28 21:27:30 +0200

    text

--- admin.php
+++ admin.php
@@ -1,19 +1,28 @@
 <?php
+session_start();
+
 require_once 'database/database.php';
+$sql="SELECT* FROM  `articles`";
+$query=$pdo->prepare($sql);
+$query->execute();
+$articles=$query->fetchAll();
 
-// 1--On affiche le titre autre
 
-$pageTitle ='page admin'; 
+// 1-On affiche le titre
+
+$pageTitle = "page dashbord";
 
 // 2-Debut du tampon de la page de sortie
- 
-ob_start();
 
-// 3-inclure le layout de la page d' admin
+ob_start(); //ce qui veut dire que le navigateur va stocker le contenu de la page dans un tampon
+
+// 3-inclure le layout de la page register
 require_once 'layouts/admin_dashboarddddddddddddddddd/admin_dashboarddddddddddddddddd_html.php';
 
-//4-recuperation du contenu du tampon de la page d'admin
+//4-recuperation du contenu du tampon de la page register
 $pageContent = ob_get_clean();
 
 //5-Inclure le layout de la page de sortie
 require_once 'layouts/layout_html.php';
+
+?>
--- layouts/admin_dashboarddddddddddddddddd/admin_dashboarddddddddddddddddd_html.php
+++ layouts/admin_dashboarddddddddddddddddd/admin_dashboarddddddddddddddddd_html.php
@@ -1,17 +1,14 @@
 <section style="display: flex; flex-direction:column;  justify-content:center; align-items:center">
-    <div class="header">
-        <h1 style="width:400px ;">creer un article</h1>
-    </div>
-    <form method="post" class="form" id="form"
+
+    <h1 style="width:90vw ;">creer un article</h1>
+
+    <form method="post" style="width:100%;" id="form"
         enctype="multipart/form-data">
         <div class="form-control">
             <label for="username">Titre de l'article</label>
             <input type="text" id="title" placeholder="Le developpement mobile" name="title" autocomplete="off">
         </div>
-        <div class="form-control">
-            <label for="slug">slug</label>
-            <input type="text" name="slug" id="slug" placeholder="le-developpement-mobile">
-        </div>
+
         <div class="form-control">
             <label for="text">introduction</label>
             <input type="text" id="text" placeholder="L'introduction de l'article" name="introduction">
@@ -30,23 +27,29 @@
   border-radius: var(--radius);
   cursor: pointer;">Creer l'article</button>
         </div>
-        <div class="form-control">
-            <a href="index2.php" style="  width: 70%;
-  padding: var(--sp-sm);
-  font-size: var(--fs-6);
-  background: var(--color-first);
-  color: var(--color-white);
-  border: none;
-  border-radius: var(--radius);
-  cursor: pointer;">voir </a>
-        </div>
+
     </form>
 </section>
-
-
 <section>
-    <!-- <a name="delete" href="delete.php?id=<?= $article['id'] ?>" 
-       onClick="return confirm('Voulez-vous vraiment supprimer cet article ?');">Supprimer</a>
-    <a href="">Éditer</a> -->
-
+<h2>Listes des articles!!!</h2>
+<div class="container">
+    <?php 
+    foreach($articles as $article):?>
+    <div class="box">
+        <h2><?= $article['title'] ?></h2>
+        <p><?php $article['introduction'] ?></p>
+        <div class="link" style="display: flex; gap:10px;">
+        <div class="">
+            <a href="">Voir plus</a>
+    </div>
+        <div class="">
+            <a href="">editer</a>
+    </div>
+        <div class="">
+            <a href="">supprimer</a>
+    </div>
+    </div>
+    </div>
+    <?php endforeach; ?>
+</div>
 </section>
