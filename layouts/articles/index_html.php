f7b8c9a30261c7a422f4e6e77627d0e5912de768
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-28 16:23:20 +0200

    text

--- layouts/articles/index_html.php
+++ layouts/articles/index_html.php
@@ -1,3 +1,4 @@
+
 <h1>Listes des articles</h1>
 
 <hr>
@@ -6,15 +7,15 @@
   foreach ($articles as $article): 
     ?>
     <div>
-    <h2> <?= $article['title'] ?> </h2>
+    <h2> <?= htmlspecialchars($article['title']) ?> </h2>
 
-  <p> <?= $article['introduction'] ?> </p>
+    <p> <?= htmlspecialchars($article['introduction']) ?> </p>
 
-  <a href="article.php?id=<?= $article['id'] ?>">Voir plus</a>
-  </div>
-  <hr>
-  <?php  endforeach;  ?>
-
-
-  
+    <a href="article.php?id=<?= urlencode($article['id']) ?>">Voir plus</a>
+    <a name="delete" href="delete.php?id=<?= urlencode($article['id']) ?>" 
+       onClick="return confirm('Voulez-vous vraiment supprimer cet article ?');">Supprimer</a>
+    <a href="edit-article.php?id=<?= urlencode($article['id']) ?>">Éditer</a>
+    </div>
+    <hr>
+  <?php endforeach; ?>
 </div>
