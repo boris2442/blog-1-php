f52b4c341654e7cedf75d5a15165e3098eeda9bb
boris2442 <aubinborissimotsebo@gmail.com>
2025-05-07 19:07:01 +0200

    text

--- layouts/articles/show_html.php
+++ layouts/articles/show_html.php
@@ -6,9 +6,7 @@
         <p> <em>Publié le : <?= $article['created_at'] ?></em></p>
         <p>Mis à jour le : <?= $article['updated_at'] ?></p>
 
-        <form class="form">
-            <button> <a href="index.php" style="color:white ;text-decoration:none" ;>Retour</a></button>
-        </form>
+
         <?php
         if (count($commentaires) === 0): ?>
 
@@ -17,27 +15,35 @@
             <h2 class="comment-heading">Il y a déjà <?= count($commentaires) ?> réactions :</h2>
             <?php foreach ($commentaires as $commentaire): ?>
 
-                <h3 class="comment-author">Commentaire de : <?= $commentaire['username']?></h3>
+                <h3 class="comment-author">Commentaire de : <?= $commentaire['username'] ?></h3>
                 <small class="comment-date">creer le:<?= $commentaire['created_at'] ?></small>
                 <blockquote class="comment-content">
                     <em><?= $commentaire['content'] ?></em>
                 </blockquote>
+                <hr>
             <?php endforeach; ?>
         <?php endif; ?>
 
         <!-- Formulaire de commentaire -->
-        <form method="POST" class="comment-form" action="save-comment">
-            <h3 class="comment-form-heading">Vous voulez réagir ? N'hésitez pas !</h3><br>
+        <?php if (isset($_SESSION['auth'])) : ?>
+            <form method="POST" class="comment-form" action="save-comment">
+                <h3 class="comment-form-heading">Vous voulez réagir ? N'hésitez pas !</h3><br>
 
-            <textarea name="content" cols="30" rows="10" placeholder="Votre commentaire ..."
-                class="comment-form-content" style="resize: none;"></textarea><br>
-            <input type="number" name="article_id" value="<?= $article_id ?>" hidden><br>
-            <input type="hidden" name="user_id" value="<?= $_SESSION['auth']['id'] ?>"><br>
-            <button class="comment-form-submit" type="submit">COMMENTER !</button><br>
-        </form>
-
-        <p>Veuillez vous connecter ou vous inscrire pour commenter.</p>
-        <a href="register.php">S'inscrire</a> | <a href="login.php">Se connecter</a>
+                <textarea name="content" cols="30" rows="10" placeholder="Votre commentaire ..."
+                    class="comment-form-content" style="resize: none;"></textarea><br>
+                <input type="number" name="article_id" value="<?= $article_id ?>" hidden><br>
+                <input type="hidden" name="user_id" value="<?= $_SESSION['auth']['id'] ?>"><br>
+                <button class="comment-form-submit" type="submit">COMMENTER !</button><br>
+            </form>
+        <?php else :
+        ?>
+            <p>Veuillez vous connecter ou vous inscrire pour commenter.</p>
+            <a href="register.php">S'inscrire</a> | <a href="login.php">Se connecter</a>
+        <?php endif;
+        ?>
 
     </div>
+
+        <button> <a href="index.php" style="color:blue ;text-decoration:none" ;>Retour</a></button>
+
 </section>
